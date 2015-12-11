4a2a3757a8898051b81a78424dd1046dda5f20be
Streuli273 <streuli273@gmail.com>
2015-12-11 11:01:23 +0000

    add lock image to i3lock

--- .config/i3/i3lock.sh
+++ .config/i3/i3lock.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-xwobf /tmp/.i3lock.png
-i3lock -i /tmp/.i3lock.png
+xwobf /tmp/i3lock.png
+convert /tmp/i3lock.png ~/.config/i3/lock.png -gravity center -composite -matte /tmp/i3lock.png
+i3lock -i /tmp/i3lock.png
+rm /tmp/i3lock.png
