7a959353bc286f2fad374050cf7085d05efcfb25
Streuli273 <streuli273@gmail.com>
2016-05-13 12:18:37 +0100

    rofi script

---
+++ .config/i3/rofi.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rofi -show run -font "inconsolata-g 9" -color-window "#161616, #ab4642, #ab4642" -color-normal "#181818, #d8d8d8, #181818, #ab4642, #d8d8d8" -color-active "#181818, #d8d8d8, #181818, #ab4642, #d8d8d8" -color-urgent "#181818, #d8d8d8, #181818, #ab4642, #d8d8d8" -terminal termite -hide-scrollbar -opacity 80%
+
